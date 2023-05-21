69ae3e4cee45396553b581161196ee716a9094b6
Luiz Lopes <luizlopesbr@gmail.com>
2023-05-21 11:00:11 -0300

    alteracao em teste post

--- test/rotas_post_user.test.js
+++ test/rotas_post_user.test.js
@@ -2,9 +2,11 @@ const request = require('supertest');
 const app = require('../routes/usuario'); // Arquivo de rotas
 
 describe('Testes de rotas POST', () => {
-  it('/users', async () => { //Deve retornar status 200 ao acessar a rota 
-    const response = await request(app).post('/');
-    expect(response.status).toBe(200);
+  it( async () => {//Deve retornar status 201 ao criar um novo recurso
+    const response = await request(app)
+      .post('/api/users') // Rota POST
+      .send({ data: 'user, password' },); // Dados a serem enviados no corpo da requisição
+    expect(response.status).toBe(201);
   });
 
   

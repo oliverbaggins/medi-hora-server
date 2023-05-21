981fedb012f19b5e33050f046914e693b0a8b3bc
Luiz Lopes <luizlopesbr@gmail.com>
2023-05-21 10:39:45 -0300

    teste rota post usuário

--- test/rotas_post_user.test.js
+++ test/rotas_post_user.test.js
@@ -1,10 +1,12 @@
 const request = require('supertest');
 const app = require('../routes/usuario'); // Arquivo de rotas
 
-describe('Testes de rotas GET', () => {
+describe('Testes de rotas POST', () => {
   it('/users', async () => { //Deve retornar status 200 ao acessar a rota 
-    const response = await request(app).get('/');
+    const response = await request(app).post('/');
     expect(response.status).toBe(200);
   });
+
+  
 
 });

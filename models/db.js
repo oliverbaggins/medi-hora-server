fcbb215b095eb938b8d218c26bef247523a34d56
Emmanuel <emmanuelsoaressilva@gmail.com>
2023-05-25 23:38:01 -0300

    Alterando arquivo de conexão com o banco

--- models/db.js
+++ models/db.js
@@ -1,14 +1,19 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const DB_URL = 'mongodb://localhost:27017/simedic';
+const DB_URL = 'mongodb+srv://cluster0.5r3re24.mongodb.net'
 
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+  user: 'usuarioDB',
+  pass: 'fipNeuZLAePigot3',
+  dbName: 'simedic'
+})
 
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'Erro na conexão com o banco de dados:'));
-db.once('open', function() {
-  console.log('Conexão com o banco de dados estabelecida com sucesso!');
-});
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'Erro na conexão com o banco de dados:'))
+db.once('open', function () {
+  console.log('Conexão com o banco de dados estabelecida com sucesso!')
+})
+
+module.exports = db
